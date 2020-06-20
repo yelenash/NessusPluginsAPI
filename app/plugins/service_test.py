683eb40863d2675c8e47d09c18c9083ab84b9a19
unknown <yelenash13@gmail.com>
2020-06-20 22:48:26 +0300

    fix test

--- app/plugins/service_test.py
+++ app/plugins/service_test.py
@@ -10,18 +10,18 @@ class TestPluginService:
             results = PluginsService.get_all("pluginID")
             for idx, result in enumerate(results):
                 if idx > 0:
-                    assert result.id >= results[idx - 1].id
+                    assert result.id <= results[idx - 1].id
 
     def test_get_all_sort_by_published(self, app):
         with app.app_context():
             results = PluginsService.get_all("published")
             for idx, result in enumerate(results):
                 if idx > 0:
-                    assert dateutil.parser.parse(result.published) >= dateutil.parser.parse(results[idx - 1].published)
+                    assert dateutil.parser.parse(result.published) <= dateutil.parser.parse(results[idx - 1].published)
 
     def test_get_all_sort_by_score(self, app):
         with app.app_context():
             results = PluginsService.get_all("pluginID")
             for idx, result in enumerate(results):
                 if idx > 0:
-                    assert result.score >= results[idx - 1].score
+                    assert result.score <= results[idx - 1].score
